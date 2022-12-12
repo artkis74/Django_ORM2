446530f540d1b86d895323ebe8a90239d0974d81
artkis74 <kiselev_artem1991@mail.ru>
2022-12-12 09:43:07 +0500

    Solving 2# Task

--- m2m-relations/articles/models.py
+++ m2m-relations/articles/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 
 class Article(models.Model):
-
     title = models.CharField(max_length=256, verbose_name='Название')
     text = models.TextField(verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
     image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
+    tag = models.ManyToManyField("Tag", through='Scope', related_name='articles')
 
     class Meta:
         verbose_name = 'Статья'
@@ -14,3 +14,21 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=256, verbose_name='Имя тега')
+    #articles
+
+    class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+
+    def __str__(self):
+        return self.name
+class Scope(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='articles_tags')
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, related_name='articles_tags')
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_main = models.BooleanField(default=False, verbose_name='Основной')
+
